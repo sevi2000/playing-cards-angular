77069170d72efa0b3f84dfcc39e38a34238c8c88
SEVI DERVISHI <sevidervishi@outlook.fr>
2025-05-15 11:06:57 +0200

    Using properties form ts class with {{}} syntax next we will seee inputs to make component reusable

--- src/app/components/playing-card/playing-card.component.ts
+++ src/app/components/playing-card/playing-card.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class PlayingCardComponent {
 
+  name: string = "My monster";
+  hp: number = 40;
+  figureCaption: string = "N°001 Monster";
+  attackName: string = "Geo Impact";
+  attackStrength: number = 60;
+  attackDescription: string = "This is a long description of monster's attack...";
 }
