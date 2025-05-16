c8cc4ce4ca5515392f0b3b8dd2a2f9035776a5ff
SEVI DERVISHI <sevidervishi@outlook.fr>
2025-05-16 11:47:28 +0200

    We can do the same with the search part by making searchChange an input signal and seachChange using the new output() function.

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, output, Output } from '@angular/core';
+import { Component, EventEmitter, input, Input, output, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -9,8 +9,8 @@ import { FormsModule } from '@angular/forms';
 })
 export class SearchBarComponent {
 
-  @Input() search = "Initial";
-  @Output() searchChange = new EventEmitter<string>();
+  search = input <string>("Initial");
+  searchChange = output<string>();
   
   searchButtonClicked = output({alias : "submit"});
   searchClick() {
